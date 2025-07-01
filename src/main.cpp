aab16a71d91bdea09cfbe369dbc1d7040ee5d66b
Maciej Urban <maciej-urban@outlook.com>
2025-07-01 23:12:57 +0200

    feat: moving shadow!
    - Background is drawn, uses nearest neighbor filtering
    - Screen is integer scaled from 100x64

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,148 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <SDL3_image/SDL_image.h>
 #include <iostream>
+#include "number_types.h"
+#include <vector>
+#include <cmath>
 
-int SCREEN_WIDTH  = 640;
-int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH  = 100;
+const int SCREEN_HEIGHT = 64;
+const int WINDOW_WIDTH  = 1000;
+const int WINDOW_HEIGHT = 640;
 
-static SDL_Window*  window  = nullptr;
-static SDL_Surface* surface = nullptr;
+struct Img {
+    SDL_Texture* img;
+    f32          width;
+    f32          height;
+};
 
-bool init() {
+struct Entity {
+    int health = 100;
+    int damage = 20;
+    f32 speed  = 1;
+    f32 x;
+    f32 y;
+};
+
+struct Game {
+    SDL_Window*         window;
+    SDL_Renderer*       renderer;
+    Img                 bg;
+    Img                 entity_shadow;
+    Entity              player;
+    std::vector<Entity> enemies;
+};
+
+static Game g = {};
+
+// Has to be called after initializing the renderer.
+//
+// returns null on error
+static bool load_img(const char* path, Img& i) {
+    i.img = IMG_LoadTexture(g.renderer, path);
+    if (i.img == nullptr) {
+        SDL_Log("Could not load img! SDL err: %s\n", SDL_GetError());
+        return false;
+    }
+
+    bool ok = SDL_GetTextureSize(i.img, &i.width, &i.height);
+    if (!ok) {
+        SDL_Log("Could not load img size! SDL err: %s\n", SDL_GetError());
+        return false;
+    }
+
+    ok = SDL_SetTextureScaleMode(i.img, SDL_SCALEMODE_NEAREST);
+    if (!ok) {
+        SDL_Log("Could not set texture scaling to nearest neighbor! SDL err: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+static bool init() {
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         SDL_Log("SDL could not initialize! SDL err: %s\n", SDL_GetError());
         return false;
     }
 
-    window = SDL_CreateWindow("Fists of Fury", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if (window == nullptr) {
-        SDL_Log("Window could not be created! SDL err: %s\n", SDL_GetError());
-        return false;
+    {
+        g.window = SDL_CreateWindow(
+            "Fists of Fury",
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            SDL_WINDOW_RESIZABLE
+        );
+
+        if (g.window == nullptr) {
+            SDL_Log("Window could not be created! SDL err: %s\n", SDL_GetError());
+            return false;
+        }
     }
 
-    surface = SDL_GetWindowSurface(window);
+    {
+        g.renderer = SDL_CreateRenderer(g.window, nullptr);
+        if (g.renderer == nullptr) {
+            SDL_Log("Renderer could not be created! SDL err: %s\n", SDL_GetError());
+            return false;
+        }
+
+        bool ok = SDL_SetRenderLogicalPresentation(
+            g.renderer,
+            SCREEN_WIDTH,
+            SCREEN_HEIGHT,
+            SDL_LOGICAL_PRESENTATION_INTEGER_SCALE
+        );
+        if (!ok) {
+            SDL_Log("Could not set logical presentation! SDL err: %s\n", SDL_GetError());
+            return false;
+        }
+    }
+
+    {
+        bool ok = load_img("assets/art/backgrounds/street-background.png", g.bg);
+        if (!ok) {
+            SDL_Log("Failed to load bg img! SDL err: %s\n", SDL_GetError());
+            return false;
+        }
+    }
+
+    {
+        bool ok = load_img("assets/art/characters/shadow.png", g.entity_shadow);
+        if (!ok) {
+            SDL_Log("Failed to load shadow img! SDL err: %s\n", SDL_GetError());
+            return false;
+        }
+    }
+
+    {
+        // player setup
+        g.player.x = SCREEN_WIDTH  / 2;
+        g.player.y = SCREEN_HEIGHT / 2;
+    }
+
+    {
+        // enemies setup
+    }
+
     return true;
 }
 
-void close() {
-    SDL_DestroyWindow(window);
-    window = nullptr;
-    surface = nullptr;
-    SDL_Quit();
+static void draw_background(SDL_Renderer* r) {
+    const SDL_FRect dst = {0, 0, g.bg.width, g.bg.height};
+    SDL_RenderTexture(r, g.bg.img, NULL, &dst);
+}
+
+static void draw_entity(SDL_Renderer* r, Entity e) {
+    const SDL_FRect dst = {e.x, e.y, g.entity_shadow.width, g.entity_shadow.height};
+    SDL_RenderTexture(r, g.entity_shadow.img, NULL, &dst);
+}
+
+static void update_enemy(Entity* e) {}
+
+static void update_player(Entity* p) {
+    p->x = std::fmod(p->x + 1, (f32)SCREEN_WIDTH);
 }
 
 int main(int argc, char* argv[]) {
@@ -46,15 +160,19 @@ int main(int argc, char* argv[]) {
             }
         }
 
-        SDL_FillSurfaceRect(
-            surface,
-            nullptr,
-            SDL_MapSurfaceRGB(surface, 0xFF, 0xFF, 0xFF)
-        );
+        SDL_RenderClear(g.renderer);
+        draw_background(g.renderer);
 
-        SDL_UpdateWindowSurface(window);
+        for (int idx = 0; idx < g.enemies.size(); idx++) {
+            update_enemy(&g.enemies[idx]);
+            draw_entity(g.renderer, g.enemies[idx]);
+        }
+
+        update_player(&g.player);
+        draw_entity(g.renderer, g.player);
+
+        SDL_RenderPresent(g.renderer);
     }
 
-    close();
     return 0;
 }
