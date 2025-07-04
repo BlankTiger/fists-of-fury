4462d43d13b1748226275e05eb7e1f9682846615
Maciej Urban <maciej-urban@outlook.com>
2025-07-04 19:48:19 +0200

    feat: sprite loading, management and rendering

--- src/sprite.cpp
+++ src/sprite.cpp
@@ -1,5 +1,7 @@
 #include <SDL3_image/SDL_image.h>
+#include <SDL3/SDL.h>
 #include "sprite.h"
+#include <cassert>
 
 bool img_load(Img& i, SDL_Renderer* r, const char* path) {
     i.img = IMG_LoadTexture(r, path);
@@ -23,3 +25,36 @@ bool img_load(Img& i, SDL_Renderer* r, const char* path) {
     return true;
 }
 
+bool sprite_load(Sprite& s, SDL_Renderer* r, const char* path) {
+    assert(s.max_frames_in_row_count   > 0);
+    assert(s.frames_in_each_row.size() > 0);
+
+    return img_load(s.img, r, path);
+}
+
+Src_Rect_With_Size sprite_get_src_rect(const Sprite& s, u32 row, u32 col) {
+    assert(row < s.frames_in_each_row.size());
+    assert(col < s.frames_in_each_row[row]);
+    f32 x = 5;
+    f32 y = 5;
+    f32 width = 10;
+    f32 height = 10;
+    return {
+        .src    = { x, y, width, height },
+        .width  = 8.,
+        .height = 8.,
+    };
+}
+
+bool sprite_draw_at_dst(const Sprite& s, SDL_Renderer* r, f32 x_dst, f32 y_dst, u32 row, u32 col) {
+    assert(row < s.frames_in_each_row.size());
+    assert(col < s.frames_in_each_row[row]);
+
+    const f32 width = s.img.width / s.max_frames_in_row_count;
+    const f32 height = s.img.height / s.frames_in_each_row.size();
+    const f32 x = col * width;
+    const f32 y = row * height;
+    const SDL_FRect src = {x, y, width, height};
+    const SDL_FRect dst = {x_dst, y_dst, width, height};
+    return SDL_RenderTexture(r, s.img.img, &src, &dst);
+}
--- src/sprite.h
+++ src/sprite.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include <span>
+
 #include "number_types.h"
 #include "array_view.h"
 
+// forward declaration to omit including SDL3
 struct SDL_Texture;
+struct SDL_Renderer;
+struct SDL_FRect;
 
 struct Img {
     SDL_Texture* img;
@@ -11,17 +16,27 @@ struct Img {
     f32          height;
 };
 
-struct Sprite {
-    Img             img;
-    u32             frame_width;
-    u32             frame_height;
-    Array_View<u32> frames_in_each_row;
-};
-
-
-struct SDL_Renderer;
-
 // Has to be called after initializing the renderer.
 //
 // returns false on error
 bool img_load(Img& i, SDL_Renderer* r, const char* path);
+
+struct Sprite {
+    Img                  img;
+    u32                  max_frames_in_row_count;
+    std::span<const u32> frames_in_each_row;
+};
+
+// Asserts that `max_frames_in_row_count` and `frames_in_each_row`
+// are already initialized.
+bool sprite_load(Sprite& s, SDL_Renderer* r, const char* path);
+
+struct Src_Rect_With_Size {
+    SDL_FRect src;
+    f32       width;
+    f32       height;
+};
+
+Src_Rect_With_Size sprite_get_src_rect(const Sprite& s, u32 row, u32 col);
+
+bool sprite_draw_at_dst(const Sprite& s, SDL_Renderer* r, f32 x_dst, f32 y_dst, u32 row, u32 col);
