9e337dcc9febe0e04ba42de75cb9d85fac199e08
Maciej Urban <maciej-urban@outlook.com>
2025-07-05 19:17:14 +0200

    refactor: level_info.h simplification

--- src/level_info.h
+++ src/level_info.h
@@ -16,48 +16,38 @@ enum struct Level : usize {
 static_assert((usize)Level::Street == 0);
 static_assert((usize)Level::Bar    == 1);
 
+enum struct Border : usize {
+    Top,
+    Bottom,
+    Left,
+    Right
+};
+
 struct Level_Info {
-    const char*               bg_path;
-    std::array<SDL_FRect, 10> collision_boxes;
-    usize                     box_count;
+    std::array<SDL_FRect, 4> collision_boxes;
+    const char*              bg_path;
 };
 
 std::span<const SDL_FRect> level_info_boxes(const Level_Info& li) {
-    return std::span(li.collision_boxes.data(), li.box_count);
-}
-
-constexpr Level_Info make_level_info(const char* path, std::initializer_list<SDL_FRect> boxes) {
-    Level_Info info{};
-    info.bg_path = path;
-    info.box_count = boxes.size();
-
-    usize i = 0;
-    for (const auto& box : boxes) {
-        info.collision_boxes[i++] = box;
-    }
-
-    return info;
+    return std::span(li.collision_boxes);
 }
 
 constexpr std::array<Level_Info, (usize)Level::Count> level_data = []() {
     std::array<Level_Info, (usize)Level::Count> data{};
 
-    data[(usize)Level::Street] = make_level_info(
-        "assets/art/backgrounds/street-background.png",
-        {
-            /* TOP    */ {0,  0,  150, 32},
-            /* LEFT   */ {-1, 0,  1,   64},
-            /* BOTTOM */ {0,  64, 150, 10}
-        }
-    );
+    data[(usize)Level::Street] = {};
+    data[(usize)Level::Street].bg_path  = "assets/art/backgrounds/street-background.png";
+    data[(usize)Level::Street].collision_boxes[(usize)Border::Top]    = {0,   0,  150, 32};
+    data[(usize)Level::Street].collision_boxes[(usize)Border::Bottom] = {0,   64, 150, 10};
+    data[(usize)Level::Street].collision_boxes[(usize)Border::Left]   = {-1,  0,  1,   64};
+    data[(usize)Level::Street].collision_boxes[(usize)Border::Right]  = {400, 0,  1,   64};
 
-    data[(usize)Level::Bar] = make_level_info(
-        "assets/art/backgrounds/bar-background.png",
-        {
-            {10, 20, 50, 30},
-            {100, 150, 40, 60}
-        }
-    );
+    data[(usize)Level::Bar] = {};
+    data[(usize)Level::Bar].bg_path  = "assets/art/backgrounds/bar-background.png";
+    data[(usize)Level::Bar].collision_boxes[(usize)Border::Top]    = {0,   0,  150, 32};
+    data[(usize)Level::Bar].collision_boxes[(usize)Border::Bottom] = {0,   64, 150, 10};
+    data[(usize)Level::Bar].collision_boxes[(usize)Border::Left]   = {-1,  0,  1,   64};
+    data[(usize)Level::Bar].collision_boxes[(usize)Border::Right]  = {400, 0,  1,   64};
 
     return data;
 }();
