d4cedc3a56e67b2d49718a3628684b9d5625c8d8
Maciej Urban <maciej-urban@outlook.com>
2025-07-12 12:57:49 +0200

    feat: vec2 + operator

--- src/vec2.h
+++ src/vec2.h
@@ -3,4 +3,16 @@
 template<typename T>
 struct Vec2 {
     T x, y;
+
+    Vec2<T> operator+(const Vec2<T>& other) const {
+        return {x + other.x, y + other.y};
+    }
+
+    Vec2<T>& operator+=(const Vec2<T>& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
 };
+
+template struct Vec2<f32>;
