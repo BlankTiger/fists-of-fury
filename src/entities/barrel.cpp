c03b9537bc5e26df72ebbbae4e03350e3e217368
Maciej Urban <maciej-urban@outlook.com>
2025-07-14 19:16:38 +0200

    feat: draw destroyed barrel when barrel state = destroyed

--- src/entities/barrel.cpp
+++ src/entities/barrel.cpp
@@ -55,8 +55,19 @@ void barrel_draw(SDL_Renderer* r, const Entity& e, const Game& g) {
     const Vec2<f32> drawing_coords = entity_offset_to_bottom_center(e);
     const Vec2<f32> screen_coords = game_get_screen_coords(g, drawing_coords);
 
-    bool ok = sprite_draw_at_dst(g.sprite_barrel, r, screen_coords.x, screen_coords.y, 0, 0, SDL_FLIP_NONE);
-    if (!ok) SDL_Log("Failed to draw barrel sprite! SDL err: %s\n", SDL_GetError());
+    switch (e.extra_barrel.state) {
+        case (Barrel_State::Idle): {
+            bool ok = sprite_draw_at_dst(g.sprite_barrel, r, screen_coords.x, screen_coords.y, 0, 0, SDL_FLIP_NONE);
+            if (!ok) SDL_Log("Failed to draw barrel sprite! SDL err: %s\n", SDL_GetError());
+            break;
+        }
+
+        case (Barrel_State::Destroyed): {
+            bool ok = sprite_draw_at_dst(g.sprite_barrel, r, screen_coords.x, screen_coords.y, 0, 1, SDL_FLIP_NONE);
+            if (!ok) SDL_Log("Failed to draw barrel sprite! SDL err: %s\n", SDL_GetError());
+            break;
+        }
+    }
 
     const Vec2<f32> world_coords = {e.x, e.y};
     draw_shadow(r, world_coords, e.shadow_offsets, g);
