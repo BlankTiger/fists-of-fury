d005269a2a8f9b5abbf315ed863b82aedc63d455
Maciej Urban <maciej-urban@outlook.com>
2025-07-15 21:40:12 +0200

    refactor: drawing boxes

--- src/draw.cpp
+++ src/draw.cpp
@@ -1,14 +1,14 @@
 #include "draw.h"
 #include "settings.h"
 
-internal void _draw_box(SDL_Renderer* r, const SDL_FRect& box, const f32 colors[4]) {
+static void _draw_box(SDL_Renderer* r, const SDL_FRect& box, const std::array<f32, 4> colors_border, const std::array<f32, 4> colors_fill) {
     SDL_SetRenderDrawBlendMode(r, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(r, colors[0], colors[1], colors[2], colors[3]);
+    SDL_SetRenderDrawColor(r, colors_border[0], colors_border[1], colors_border[2], colors_border[3]);
     bool ok = SDL_RenderRect(r, &box);
     if (!ok) SDL_Log("Failed to draw box! SDL err: %s\n", SDL_GetError());
 
     SDL_SetRenderDrawBlendMode(r, SDL_BLENDMODE_BLEND);
-    SDL_SetRenderDrawColor(r, colors[0] / 2, colors[1] / 2, colors[2] / 2, colors[3] / 2);
+    SDL_SetRenderDrawColor(r, colors_fill[0], colors_fill[1], colors_fill[2], colors_fill[3]);
     ok = SDL_RenderFillRect(r, &box);
     if (!ok) SDL_Log("Failed to draw box! SDL err: %s\n", SDL_GetError());
 
@@ -22,7 +22,7 @@ void draw_collision_box(SDL_Renderer* r, const Vec2<f32>& world_coords, const SD
         offsets.w,
         offsets.h
     };
-    _draw_box(r, collision_box_screen, settings.collision_box_colors);
+    _draw_box(r, collision_box_screen, settings.collision_box_colors_border, settings.collision_box_colors_fill);
 }
 
 void draw_hurtbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& hurtbox_offsets, const Game& g) {
@@ -32,7 +32,7 @@ void draw_hurtbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRec
         hurtbox_offsets.w,
         hurtbox_offsets.h
     };
-    _draw_box(r, hurtbox_screen, settings.hurtbox_colors);
+    _draw_box(r, hurtbox_screen, settings.hurtbox_colors_border, settings.hurtbox_colors_fill);
 }
 
 void draw_hitbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& hitbox_offsets, const Game& g) {
@@ -42,24 +42,24 @@ void draw_hitbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect
         hitbox_offsets.w,
         hitbox_offsets.h
     };
-    _draw_box(r, hitbox_screen, settings.hitbox_colors);
+    _draw_box(r, hitbox_screen, settings.hitbox_colors_border, settings.hitbox_colors_fill);
 }
 
 void draw_level(SDL_Renderer* r, const Game& g) {
     const SDL_FRect dst = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
     SDL_RenderTexture(r, g.bg.img, &g.camera, &dst);
-    #if SHOW_COLLISION_BOXES
-    for (const auto& box : level_info_get_collision_boxes(g.curr_level_info)) {
-        // Draw collision boxes relative to camera
-        SDL_FRect screen_box = {
-            box.x - g.camera.x,
-            box.y - g.camera.y,
-            box.w,
-            box.h
-        };
-        _draw_box(r, screen_box, COLLISION_BOX_COLORS);
+    if (settings.show_collision_boxes) {
+        for (const auto& box : level_info_get_collision_boxes(g.curr_level_info)) {
+            // Draw collision boxes relative to camera
+            SDL_FRect screen_box = {
+                box.x - g.camera.x,
+                box.y - g.camera.y,
+                box.w,
+                box.h
+            };
+            _draw_box(r, screen_box, settings.collision_box_colors_border, settings.collision_box_colors_fill);
+        }
     }
-    #endif
 }
 
 void draw_shadow(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& offsets, const Game& g) {
@@ -71,4 +71,8 @@ void draw_shadow(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect
     };
     bool ok = SDL_RenderTexture(r, g.entity_shadow.img, NULL, &shadow_box_screen);
     if (!ok) SDL_Log("Failed to draw shadow! SDL err: %s\n", SDL_GetError());
+}
+
+void draw_box(SDL_Renderer* r, const SDL_FRect dst, const Draw_Box_Opts& opts) {
+    _draw_box(r, dst, opts.colors_border, opts.colors_fill);
 }
--- src/draw.h
+++ src/draw.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <array>
+
 #include <SDL3/SDL.h>
+
 #include "game.h"
 #include "vec2.h"
 
@@ -9,3 +12,10 @@ void draw_shadow(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect
 void draw_collision_box(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& collision_box_offsets, const Game& g);
 void draw_hurtbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& hurtbox_offsets, const Game& g);
 void draw_hitbox(SDL_Renderer* r, const Vec2<f32>& world_coords, const SDL_FRect& hitbox_offsets, const Game& g);
+
+struct Draw_Box_Opts {
+    std::array<f32, 4> colors_border;
+    std::array<f32, 4> colors_fill;
+};
+
+void draw_box(SDL_Renderer* r, const SDL_FRect dst, const Draw_Box_Opts& opts);
