4f9de7150c26570da0a76c8159602482984d1a5b
Maciej Urban <maciej-urban@outlook.com>
2025-07-26 17:28:50 +0200

    refactor: group by category in settings

--- src/settings.h
+++ src/settings.h
@@ -26,19 +26,24 @@ struct Settings {
 
     std::array<f32, 4> color_text                  = {0,        0,        0,        255};
 
+    f32 ground_level                         = 0.0f;
     f32 font_size_default                    = 9.0f;
     f32 fps_max                              = 144.0f;
+
     f32 gravity                              = 0.00038f;
     f32 jump_velocity                        = -0.15f;
+    u64 player_combo_timeout_ms              = 1000;
+
     f32 barrel_jump_velocity                 = -0.10f;
     f32 barrel_knockback_velocity            = 0.05f;
+
     f32 enemy_knockback_velocity             = 0.04f;
     f32 enemy_knockdown_velocity             = 0.1f;
     f32 enemy_flying_back_velocity           = 0.1f;
     f32 enemy_friction                       = 0.003f;
     f32 enemy_flying_back_dmg_collateral_dmg = 20.0f;
-    f32 ground_level                         = 0.0f;
-    u64 player_combo_timeout_ms              = 1000;
+    u64 enemy_attack_timeout_ms              = 2000;
+
 };
 
 extern Settings settings;
